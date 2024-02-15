c1b6a90d4e9cdc43d4f197e7a3b8dbdc2adc67af
khushi303 <maankhushi303@gmail.com>
2024-02-15 10:57:29 +0530

    change in animation

--- src/components/Utility.jsx
+++ src/components/Utility.jsx
@@ -10,27 +10,27 @@ const Utility = () => {
                 <p className='text-[16px] font-normal leading-[151%] text-black text-center mx-auto max-w-[728px] opacity-70'>Now, you might be wondering, what can I do with TROLLMP? Well, let me tell you, the possibilities are endless! You can use TROLLMP to vote on your favorite memes, tip your favorite creators, or even trade them on the open market. It's freedom, folks, freedom like you've never seen before!</p>
                 <div className='max-w-[717px] mx-auto lg:mt-[70px] sm:mt-[44px] mt-[38px] relative'>
                     <div className='relative'>
-                        <div data-aos="zoom-in" className='lg:pl-[68px] pl-[50px] lg:pb-[43px] pb-[28px] relative'>
-                            <p className='text-[18px] font-bold text-black leading-[124%] mb-[8px]'>Voting Rights</p>
-                            <p className='text-[16px] font-normal leading-[152%] text-black opacity-70'>Holders of TROLLMP will have the opportunity to participate in governance proposals, enabling them to vote on key decisions related to the development and direction of the TROLLMP ecosystem.</p>
+                        <div className='lg:pl-[68px] pl-[50px] lg:pb-[43px] pb-[28px] relative'>
+                            <p data-aos="zoom-in" className='text-[18px] font-bold text-black leading-[124%] mb-[8px]'>Voting Rights</p>
+                            <p data-aos="zoom-in" className='text-[16px] font-normal leading-[152%] text-black opacity-70'>Holders of TROLLMP will have the opportunity to participate in governance proposals, enabling them to vote on key decisions related to the development and direction of the TROLLMP ecosystem.</p>
                             <div className='absolute top-0 left-0 z-[2]'>
                                 <div className='w-[32px] h-[34px] rounded-[50%] flex items-center justify-center bg-[#DF4741]'>
                                     <div className='w-[12px] h-[12px] rounded-[50%] bg-white'></div>
                                 </div>
                             </div>
                         </div>
-                        <div data-aos="zoom-in" className='lg:pl-[68px] pl-[50px] lg:pb-[43px] pb-[28px]  relative'>
-                            <p className='text-[18px] font-bold text-black leading-[124%] mb-[8px]'>Meme Creation Rewards</p>
-                            <p className='text-[16px] font-normal leading-[152%] text-black opacity-70'>TROLLMP holders will be eligible to receive rewards for creating and sharing high-quality memes within the TROLLMP community, encouraging active participation and engagement.</p>
+                        <div className='lg:pl-[68px] pl-[50px] lg:pb-[43px] pb-[28px]  relative'>
+                            <p data-aos="zoom-in" className='text-[18px] font-bold text-black leading-[124%] mb-[8px]'>Meme Creation Rewards</p>
+                            <p data-aos="zoom-in" className='text-[16px] font-normal leading-[152%] text-black opacity-70'>TROLLMP holders will be eligible to receive rewards for creating and sharing high-quality memes within the TROLLMP community, encouraging active participation and engagement.</p>
                             <div className='absolute top-0 left-0 z-[2]'>
                                 <div className='w-[32px] h-[34px] rounded-[50%] flex items-center justify-center bg-[#DF4741]'>
                                     <div className='w-[12px] h-[12px] rounded-[50%] bg-white'></div>
                                 </div>
                             </div>
                         </div>
-                        <div data-aos="zoom-in" className='lg:pl-[68px] pl-[50px] lg:pb-[43px] pb-[28px]  relative'>
-                            <p className='text-[18px] font-bold text-black leading-[124%] mb-[8px]'>Access to Platform Features</p>
-                            <p className='text-[16px] font-normal leading-[152%] text-black opacity-70'>TROLLMP will unlock exclusive features and privileges within the TROLLMP platform, such as access to premium content, special events, and enhanced voting capabilities.</p>
+                        <div className='lg:pl-[68px] pl-[50px] lg:pb-[43px] pb-[28px]  relative'>
+                            <p data-aos="zoom-in" className='text-[18px] font-bold text-black leading-[124%] mb-[8px]'>Access to Platform Features</p>
+                            <p data-aos="zoom-in" className='text-[16px] font-normal leading-[152%] text-black opacity-70'>TROLLMP will unlock exclusive features and privileges within the TROLLMP platform, such as access to premium content, special events, and enhanced voting capabilities.</p>
                             <div className='absolute top-0 left-0 z-[2]'>
                                 <div className='w-[32px] h-[34px] rounded-[50%] flex items-center justify-center bg-[#DF4741]'>
                                     <div className='w-[12px] h-[12px] rounded-[50%] bg-white'></div>
@@ -39,9 +39,9 @@ const Utility = () => {
                         </div>
                         <div className='absolute w-[8px] top-0 bottom-0 left-[12px] z-[1] h-full bg-[#DF4741]'></div>
                     </div>
-                    <div data-aos="zoom-in" className='lg:pl-[68px] pl-[50px] relative'>
-                        <p className='text-[18px] font-bold text-black leading-[124%] mb-[8px]'>Liquidity Provision</p>
-                        <p className='text-[16px] font-normal leading-[152%] text-black opacity-70'>TROLLMP holders can contribute to liquidity pools on decentralized exchanges, facilitating trading and liquidity provision for the TROLLMP token.</p>
+                    <div className='lg:pl-[68px] pl-[50px] relative'>
+                        <p data-aos="zoom-in" className='text-[18px] font-bold text-black leading-[124%] mb-[8px]'>Liquidity Provision</p>
+                        <p data-aos="zoom-in" className='text-[16px] font-normal leading-[152%] text-black opacity-70'>TROLLMP holders can contribute to liquidity pools on decentralized exchanges, facilitating trading and liquidity provision for the TROLLMP token.</p>
                         <div className='absolute top-0 left-0 z-[2]'>
                             <div className='w-[32px] h-[34px] rounded-[50%] flex items-center justify-center bg-[#DF4741]'>
                                 <div className='w-[12px] h-[12px] rounded-[50%] bg-white'></div>
